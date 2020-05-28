881e534bdacd261ad6c1be24c9c807e273a0fbf2
Juhana Kuparinen <juhku@elisanet.fi>
2020-05-28 18:30:54 +0300

    Testit valmiit. Osa4 kappaleen b tehtävät tehty.

--- osa4/4.1-4.x/controllers/blogs.js
+++ osa4/4.1-4.x/controllers/blogs.js
@@ -6,13 +6,59 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  response.json(blog.toJSON())
+})
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const blog = new Blog(request.body)
+  if(!body.title || !body.url){
+    return response.status(400).json({ 
+      error: 'Title or url missing' 
+    })
+  }
 
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: typeof body.likes === 'number' ? body.likes : 0 
+  })
+  
   const savedBlog = await blog.save()
   response.json(savedBlog.toJSON())
+
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    console.log(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  if(!body.title || !body.url){
+    return response.status(400).json({ 
+      error: 'Title or url missing' 
+    })
+  }
+  
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog.toJSON())
 })
 
 module.exports = blogsRouter
--- osa4/4.1-4.x/tests/blog_api.test.js
+++ osa4/4.1-4.x/tests/blog_api.test.js
@@ -57,27 +57,105 @@ describe('Correct id-name', () => {
 })
 
 describe('Post', () => {
-    test('Post works', async () => {
-        const before = await api.get('/api/blogs')
 
+    let newBlog = {
+        "title": "Hmmmm",
+        "author": "Juhana Kuparinen",
+        "url": "http://localhost:3003/api/blogs",
+        "likes": 0
+    }
+
+    test('Post works', async () => {
         await api
             .post('/api/blogs')
-            .send({
-                "title": "Hmmmm",
-                "author": "Juhana Kuparinen",
-                "url": "http://localhost:3003/api/blogs",
-                "likes": 666
-            })
+            .send(newBlog)
             .expect(200)
             .expect('Content-Type', /application\/json/)
-
-        const after = await api.get('/api/blogs')
-
-        console.log(after.body)
 
         //Is there a new blog?
         const blogsAtEnd = await helper.blogsInDb()  
         expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    })
+    test('Empty likes field gets a value zero...', async () => {
+        newBlog = {
+            "title": "Zero test",
+            "author": "Juhana Kuparinen",
+            "url": "http://localhost:3003/api/blogs"
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd[blogsAtEnd.length-1].likes).toBe(0)
+    })
+    test('Response with status 400, if title or url are missing...', async () => {
+        newBlog = {
+            "author": "Juhana Kuparinen"
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
+})
+
+describe('Delete', () => {
+    test('Delete works', async () => { 
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(
+        helper.initialBlogs.length - 1
+      )
+
+      const titles = blogsAtEnd.map(r => r.title)
+
+      expect(titles).not.toContain(blogToDelete.title)
+    })
+})
+
+describe('Put', () => {
+
+    let newBlog = {
+        "title": "juuu u",
+        "author": "Juhana Kuparinen",
+        "url": "http://localhost:3003/api/blogs",
+        "likes": 8
+    }
+
+    test('Put works', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToUpdate = blogsAtStart[0]
+
+        await api
+            .put(`/api/blogs/${blogToUpdate.id}`)
+            .send(newBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd[0].title).toBe("juuu u")
+    })
+    test('Response with status 400, if title or url are missing...', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToUpdate = blogsAtStart[0]
+
+        newBlog = {
+            "author": "Juhana Kuparinen"
+        }
+        await api
+            .put(`/api/blogs/${blogToUpdate.id}`)
+            .send(newBlog)
+            .expect(400)
     })
 })
 
